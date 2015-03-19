bfffbb006398e816d122516dddb313ff4fd7ec0f
Maxim M. Kostrikin <mkostrikin@gmail.com>
2015-03-19 17:49:03 +0600

    Touch was bad idea

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 # Or set SMTPSERVER host:ip to valid smtp
 [[ -v SMTPSERVER ]] || SMTPSERVER=$(ip ro | awk '/default/ { print $3 }'):25
 /usr/bin/simpleproxy -d -L 25 -R $SMTPSERVER
-touch /data/*/conf/etc/nothing 2>/dev/null && cd /data/*/conf/etc && tar c *|tar xC /etc
+cd /data/*/conf/etc 2>/dev/null && tar c *|tar xC /etc
 [[ -v TOMCAT7PASS ]] && sed -i 's/tomcatpassfordeploy/'${TOMCAT7PASS}'/' /etc/tomcat7/tomcat-users.xml
 ls /data/*/conf/init.sh 2>/dev/null 1>/dev/null && /data/*/conf/init.sh
 
